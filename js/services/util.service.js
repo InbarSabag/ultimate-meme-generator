73ab03b6e1c05d22e3d2db8e4df0d83a9dbf85f0
InbarSabag <pe.inbar@gmail.com>
2022-08-10 21:41:04 +0300

    first day

--- js/services/util.service.js
+++ js/services/util.service.js
@@ -24,3 +24,7 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
 }
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
